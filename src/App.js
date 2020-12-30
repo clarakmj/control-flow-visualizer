bfeb66b270da331968416a2b213c8801d22502b8
xyzTiger <980401@gmail.com>
2020-12-30 00:23:31 -0800

    Refactored header and footer into separate jsx components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer'
+import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Control Flow Visualizer</h1>
-      </header>
-        <body>
-        <table>
-            <tr>
-                <th>Code</th>
-                <th>Visual</th>
-            </tr>
-            <tr>
-                <div className="Code">
-                    <textarea rows={30} cols={80}></textarea>
-                </div>
-            </tr>
-            <tr>
-                <button type="button">Run</button>
-            </tr>
-        </table>
-        </body>
-        <footer>©2020 Tiger Z & Clara K</footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <body>
+            <table>
+                <tr>
+                    <th>Code</th>
+                    <th>Visual</th>
+                </tr>
+                <tr>
+                    <div className="Code">
+                        <textarea rows={30} cols={80}></textarea>
+                    </div>
+                </tr>
+                <tr>
+                    <button type="button">Run</button>
+                </tr>
+            </table>
+            </body>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/***
+ * Header component to display title and logo
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Header() {
+    return <header className="App-header">
+        <h1>
+            Control Flow Visualizer
+        </h1>
+    </header>
+}
+
+export default Header;

b9513e7da86d2904a2108e1e758f64f269cc424e
xyzTiger <980401@gmail.com>
2021-01-01 14:35:37 -0800

    Added keywords and splitter characters, changed onChange() to print keywords detected in text

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -3,6 +3,9 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/theme-monokai";
 
+const keywords = ["if", "else", "while", "for", "return", "true", "false"]
+const splitter = new RegExp("\s+|\(|\)|[;]") //TODO: make correct RegExp (current one doesn't work)
+
 class Editor extends React.Component {
     /***
      * constructs the Editor with content set to null
@@ -23,8 +26,18 @@ class Editor extends React.Component {
      * @param newValue
      */
     onChange(newValue) {
-        console.log('change', newValue);
         this.setState({content: newValue});
+        let content = newValue.toString().split(" "); //TODO: replace with splitter
+        for (let i = 0; i < content.length; i++) {
+            if (keywords.includes(content[i])) {
+                console.log(content[i]);
+            }
+        }
+    }
+
+    //TODO: function to take array of strings to convert to an image
+    visualize(content) {
+        return 0;
     }
 
     run() {
